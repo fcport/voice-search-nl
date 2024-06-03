79249ba1b56d843e5a5af8bda549ff25cb9c261b
Federico Casadei <federico.casadei.portfolio@gmail.com>
2024-06-03 16:28:53 +0200

    feat: translations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, Signal, WritableSignal, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
 import * as RecordRTC from 'recordrtc';
 import { DomSanitizer } from '@angular/platform-browser';
+import { HF_TOKEN } from '../constants/constants';
+import { HfInference } from '@huggingface/inference';
 
 @Component({
   selector: 'app-root',
@@ -15,11 +17,12 @@ export class AppComponent {
   title = 'vocal-search-ln-2';
   listening = false;
   recorder?: RecordRTC.StereoAudioRecorder;
-  url: any;
-
-  private window: any = window;
+  url: WritableSignal<string> = signal('');
+  inference = new HfInference(HF_TOKEN);
 
   res?: MediaStreamTrack[];
+  transcription: WritableSignal<string> = signal('');
+  translation: WritableSignal<string> = signal('');
 
   async activateMic() {
     this.listening = true;
@@ -41,8 +44,38 @@ export class AppComponent {
     }
   }
 
-  processBlob(blob: Blob) {
-    this.url = URL.createObjectURL(blob);
+  async processBlob(blob: Blob) {
+    this.url.set(URL.createObjectURL(blob));
     console.log(this.url);
+
+    const resultFromHf = await this.inference.automaticSpeechRecognition({
+      data: blob,
+      model: 'openai/whisper-large-v3',
+      //@ts-ignore
+      parameters: {
+        language: 'en',
+      },
+    });
+
+    console.log(resultFromHf);
+
+    this.transcription.set(resultFromHf.text);
+
+    const translation = await this.inference.translation({
+      model: 'facebook/nllb-200-distilled-600M',
+      inputs: this.transcription(),
+      //@ts-ignore
+      parameters: {
+        src_lang: 'eng-Latn',
+        tgt_lang: 'ita_Latn',
+      },
+    });
+
+    console.log(translation);
+
+    this.translation.set(
+      (translation as unknown as { translation_text: string }).translation_text
+    );
+    console.log(this.translation());
   }
 }
